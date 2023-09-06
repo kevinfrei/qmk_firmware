f688d2ee95184f7af37d44918165dff649c372ef
Kevin Frei <kevinfrei@hotmail.com>
2023-09-06 16:19:10 -0700

    Updated for my trackball

--- keyboards/ploopyco/trackball/keymaps/freik/config.h
+++ keyboards/ploopyco/trackball/keymaps/freik/config.h
@@ -1,3 +1,6 @@
-#define PLOOPY_DPI_OPTIONS \
-        { 400, 800, 1200, 1600, 2000, 2400 }
-#define PLOOPY_DPI_DEFAULT 1
+#define PLOOPY_DPI_OPTIONS { 400, 600, 800, 1000, 1200, 1600 }
+#define PLOOPY_DPI_DEFAULT 2        // Default is 800 DPI
+#define OPT_THRES 100               // (0-1024) Threshold for activation
+#define PLOOPY_DRAGSCROLL_FIXED 1   // Just use a fixed DPI for drag-scroll
+#define PLOOPY_DRAGSCROLL_DPI 100   // Because 100 is still crazy-fast
+#define PLOOPY_DRAGSCROLL_H_INVERT  // Feels backward to me, so invert it...
